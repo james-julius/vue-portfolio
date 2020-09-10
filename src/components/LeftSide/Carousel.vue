50f0cd9923547330179aa390e8a79ce42472d7c1
james-julius <jamesdarby7@gmail.com>
2020-09-09 23:17:28 -0400

    Carousel now working. Just need to make shift dynamically with a function call

--- src/components/LeftSide/Carousel.vue
+++ src/components/LeftSide/Carousel.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="carousel">
-        <figure>
+        <figure ref="figure" :index="1">
+            <img class="cricketVisionMob" src="../../assets/cricketVisionMobile.png"/>
+            <img class="uHealthMob" src="../../assets/uHealthMobile.png"/>
+            <img class="volunteerSafeMob" src="../../assets/volunteerSafeMobile.png"/>
             <img class="cricketVisionMob" src="../../assets/cricketVisionMobile.png"/>
             <img class="uHealthMob" src="../../assets/uHealthMobile.png"/>
             <img class="volunteerSafeMob" src="../../assets/volunteerSafeMobile.png"/>
         </figure>
+    <button :click="setIndex(currentIndex)"/>
     </div>
 </template>
 
 <script>
 import './Carousel.scss';
 export default {
-    name: 'Carousel'
+    name: 'Carousel',
+    data: function() {
+        return {
+            currentIndex: 0
+        }
+    },
+    methods: {
+        setIndex: function (index) {
+            const theta = 2 * 3.141592653589793 / 6;
+            this.$refs.figure.style.transform = `rotateY(${index * theta}rad)`;
+            this.currentIndex ++;
+        }
+    }
 }
 </script>
 
