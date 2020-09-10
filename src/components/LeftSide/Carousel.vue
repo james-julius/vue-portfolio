387c38655b91e63e424e39141da45e8ff0100bce
james-julius <jamesdarby7@gmail.com>
2020-09-09 22:21:16 -0400

    Laid foundation for 3D carousel. Running into SCSS issues

---
+++ src/components/LeftSide/Carousel.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="carousel">
+        <figure>
+            <img class="cricketVisionMob" src="../../assets/cricketVisionMobile.png"/>
+            <img class="uHealthMob" src="../../assets/uHealthMobile.png"/>
+            <img class="volunteerSafeMob" src="../../assets/volunteerSafeMobile.png"/>
+        </figure>
+    </div>
+</template>
+
+<script>
+import './Carousel.scss';
+export default {
+    name: 'Carousel'
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/components/LeftSide/LeftWrapper.vue
+++ src/components/LeftSide/LeftWrapper.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="left-side">
         <!-- <iframe src="https://u-niquehealth.co.uk"/> -->
-        <img class="cricketVisionMob" src="../../assets/cricketVisionMobile.png"/>
-        <img class="uHealthMob" src="../../assets/uHealthMobile.png"/>
-        <img class="volunteerSafeMob" src="../../assets/volunteerSafeMobile.png"/> 
+        <Carousel />
     </div>
 </template>
 
 <script>
+import Carousel from './Carousel';
 export default {
     name: 'LeftSide', 
     props: {
         image: Object
+    },
+    components: {
+        Carousel
     }
 }
 </script>
@@ -20,25 +22,9 @@ export default {
 .left-side {
     height: 100%;
     width: 100%;
-    display: grid;
-    grid-template: repeat(16, 1fr) / repeat(12, 1fr);
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-    .cricketVisionMob {
-        grid-area: 2/7/10/11;
-        height: 100%;
-        width: 100%;
-    }
-    .uHealthMob {
-        grid-area: 8/2/16/6;
-        height: 100%;
-        width: 100%;
-        
-    }
-    .volunteerSafeMob {
-        grid-area: 5/5/13/9;
-        height: 100%;
-        width: 100%;
-
-    }
 }
 </style>
