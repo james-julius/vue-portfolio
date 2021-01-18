f31b6599cf89f768098194292284ba76fec4d75d
james-julius <jamesdarby7@gmail.com>
2021-01-18 08:53:24 -0800

    Updated email

--- src/components/RightSide/Homepage/Homepage.vue
+++ src/components/RightSide/Homepage/Homepage.vue
@@ -6,7 +6,7 @@
         <div class="sub-heading">
             <h3>Full-Stack Web Developer based in Los Angeles, CA</h3>
             <h4>
-                <span class="contact-icon-text"><img :src="require('@/assets/email.svg')"/> james@jamesjuliusdarby.com </span>
+                <span class="contact-icon-text"><img :src="require('@/assets/email.svg')"/> james@juliusdarby.com </span>
                 <span class="contact-icon-text"><img :src="require('@/assets/phone.svg')"/> (323) 203-9665 </span>
             </h4>
         </div>
